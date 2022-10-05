958937f97e60372f26c1b66bb79c3bc2fdfb4dcf
Emmanuel Olakotan <olakotan@gmail.com>
2022-10-05 12:56:15 +0100

    typescript effect added to <h1> with javascript DOM manipulation

--- app.js
+++ app.js
@@ -1,13 +1,19 @@
 const textDisplay = document.getElementById('text');
-const phrase = 'Hello and welcome to my portfolio, I am a <em>Full-Stack</em> web developer with an emphasis on <em>Front-End</em> development.';
+const phrase = ['Hello and welcome to my portfolio, I am a Full-Stack web developer with an emphasis on Front-End development.'];
 
-let i = 0; 
+let i = 0;
+let currentPhrase = []; 
 function loop() { 
-    if(i < phrase.length ) {
-        console.log(phrase.charAt(i));  
+textDisplay.innerHTML = currentPhrase.join('');
+    if(i < phrase[0].length) {
+        console.log(phrase[0][i]);
+        currentPhrase.push(phrase[0][i]);
         i++;     
     } 
     
-    setTimeout(loop,80);
+    setTimeout(loop,70);
+    
+  
 }
 loop()
+
