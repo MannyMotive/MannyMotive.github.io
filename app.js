78536f380601dc8b57e41dc58e7cace87db178b2
Emmanuel Olakotan <olakotan@gmail.com>
2022-10-05 01:55:51 +0100

    change FOR loop for IF statement

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
 const textDisplay = document.getElementById('text');
 const phrase = 'Hello and welcome to my portfolio, I am a <em>Full-Stack</em> web developer with an emphasis on <em>Front-End</em> development.';
 
-function loop() {
-    for(let i = 0; i < phrase.length; i++) {
-        console.log(phrase.charAt(i));
-    }
+let i = 0; 
+function loop() { 
+    if(i < phrase.length ) {
+        console.log(phrase.charAt(i));  
+        i++;     
+    } 
+    
+    setTimeout(loop,80);
 }
+loop()
